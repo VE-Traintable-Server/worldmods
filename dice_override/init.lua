de1de220f824b305fa1b6a845e29f6ed6e3d41d1
solon <solon.sandidge@gmail.com>
2026-03-24 19:47:54 -0400

    override update

--- dice_override/init.lua
+++ dice_override/init.lua
@@ -6,3 +6,9 @@ core.override_item("scifi_nodes:blue_door_closed", {use_texture_alpha = "clip"})
 core.override_item("scifi_nodes:blue_door_opened", {use_texture_alpha = "clip"})
 core.override_item("scifi_nodes:blue_door_closed_top", {use_texture_alpha = "clip"})
 core.override_item("scifi_nodes:blue_door_opened_top", {use_texture_alpha = "clip"})
+angledstairs.register_angled_stair_and_angled_slab("_white_baked_clay", "bakedclay:white",
+		{cracky = 3},
+		{"baked_clay_white.png"},
+		"White Baked Clay Stair",
+		"White Baked Clay Slab",
+		default.node_sound_stone_defaults())
